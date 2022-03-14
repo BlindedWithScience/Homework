8bf8f0b3c183ee586a9a31b15adb2fc214091993
BlindedWithScience <ernestomillan04@gmail.com>
2022-03-15 01:38:22 +0300

    Added registration/login functionality

--- main.py
+++ main.py
@@ -23,3 +23,42 @@ def write_json(contents) -> bool:
         return True
     except Exception:
         return False
+
+
+def register(data : dict, user_login : str, password : str) -> str | None:
+    if not (user_login.isalnum() and password.isalnum()):
+        return None
+
+    for user in data["users"]:
+        if user["login"] == user_login:
+            return None
+    
+    user_id = str(uuid.uuid4())
+    profile_id = str(uuid.uuid4())
+    data["users"].append(
+            {
+                "user_id": user_id,
+                "login": user_login,
+                "password": password
+            }
+    )
+    data["profiles"].append(
+            {
+                "profile_id": profile_id,
+                "user_id": user_id,
+                "level": 1
+            }
+    )
+
+    write_json(data)
+
+    return user_id
+
+
+def login(data : dict, user_login : str, password : str) -> str | None:
+    for user in data["users"]:
+        if user["login"] == user_login and user["password"] == password:
+            return user["user_id"]
+
+    return None
+
